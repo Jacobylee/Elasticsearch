1cf83e8e20a12f80d25cffe0ae4a9a28be02ef58
jacobylee <568834144@qq.com>
2021-04-19 22:09:00 +0800

    all

---
+++ evaluate.py
@@ -0,0 +1,93 @@
+import argparse
+
+from elasticsearch import Elasticsearch
+
+from embedding_service.client import EmbeddingClient
+from metrics import Score
+from utils import parse_wapo_topics
+es = Elasticsearch()
+
+
+def search(topic_id, index, k, custom, vector, q):
+    doc_ids = []
+    if not vector:
+        if custom:
+            result = es.search(index=index, size=k, body={"query": {"match": {"custom_content": q}}})
+        else:
+            result = es.search(index=index, size=k, body={"query": {"match": {"content": q}}})
+        for doc in result['hits']['hits']:
+            if doc['_source']['annotation'].split('-')[0] == topic_id:
+                doc_ids.append(int(doc['_source']['annotation'].split('-')[1]))
+            else:
+                doc_ids.append(0)
+    elif not custom:
+        if vector == "sbert_vector":
+            encoder = EmbeddingClient(host="localhost", embedding_type="sbert")
+        else:
+            encoder = EmbeddingClient(host="localhost", embedding_type="fasttext")
+        query_vector = encoder.encode([q], pooling="mean").tolist()[0]
+        # get result
+        result = es.search(index=index, size=k, body={"query": {"match": {"content": q}}})
+        doc_list = {}
+        for doc in result['hits']['hits']:
+            if vector == "sbert_vector":
+                embed_vec = doc['_source']['sbert_vector']
+            else:
+                embed_vec = doc['_source']['ft_vector']
+            cs = cosine_similarity(query_vector, embed_vec)
+            doc_list[doc['_id']] = cs
+        ordered_doc = sorted(doc_list.items(), key=lambda kv: (kv[1], kv[0]))
+        for i in ordered_doc:
+            if es.get(index=index, id=i[0])['_source']['annotation'].split('-')[0] == topic_id:
+                doc_ids.append(int(es.get(index=index, id=i[0])['_source']['annotation'].split('-')[1]))
+            else:
+                doc_ids.append(0)
+    return doc_ids
+
+
+def build_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--index_name", required=True, type=str, help="name of the ES index")
+    parser.add_argument("--topic_id", required=True, type=int, help="topic id")
+    parser.add_argument("--query_type", required=True, type=str, choices=["title", "narration", "description"],
+                        help="title, narration, description")
+    parser.add_argument('--usermode', '-u', required=False, action='store_true', help='custom content')
+    parser.add_argument("--vector_name", required=False, type=str, choices=["sbert_vector", "ft_vector"],
+                        help="sbert_vector or ft_vector")
+    parser.add_argument("--top_k", required=True, type=int, help="top k")
+    return parser.parse_args()
+
+
+def dot(l1, l2):
+    return sum(a*b for a, b in zip(l1, l2))
+
+
+def cosine_similarity(a, b):
+    return dot(a, b) / ((dot(a, a) ** .5) * (dot(b, b) ** .5))
+
+
+if __name__ == "__main__":
+    query_type_index = {"title": 0, "description": 1, "narration": 2}
+    args = build_args()
+    query = parse_wapo_topics("pa5_data/topics2018.xml")[str(args.topic_id)][query_type_index[args.query_type]]
+    searched_result = search(str(args.topic_id), args.index_name, args.top_k, args.usermode, args.vector_name, query)
+    score = Score
+    print(score.eval(searched_result, args.top_k))
+
+    # query1 = parse_wapo_topics("pa5_data/topics2018.xml")["336"][query_type_index[args.query_type]]
+    # searched_result1 = search('336', args.index_name, args.top_k, args.usermode, args.vector_name, query1)
+    #
+    # query2 = parse_wapo_topics("pa5_data/topics2018.xml")["341"][query_type_index[args.query_type]]
+    # searched_result2 = search('341', args.index_name, args.top_k, args.usermode, args.vector_name, query2)
+    #
+    # query3 = parse_wapo_topics("pa5_data/topics2018.xml")["347"][query_type_index[args.query_type]]
+    # searched_result3 = search('347', args.index_name, args.top_k, args.usermode, args.vector_name, query3)
+    #
+    # query4 = parse_wapo_topics("pa5_data/topics2018.xml")["350"][query_type_index[args.query_type]]
+    # searched_result4 = search('350', args.index_name, args.top_k, args.usermode, args.vector_name, query4)
+    #
+    # score = Score
+    # print(score.eval(searched_result1, args.top_k))
+    # print(score.eval(searched_result2, args.top_k))
+    # print(score.eval(searched_result3, args.top_k))
+    # print(score.eval(searched_result4, args.top_k))
---
+++ hw5.py
@@ -0,0 +1,105 @@
+from elasticsearch import Elasticsearch
+from flask import Flask, render_template, request
+from embedding_service.client import EmbeddingClient
+app = Flask(__name__)
+es = Elasticsearch()
+docs = []
+query_fix = ""
+index_file = "wapo_docs_50k"
+
+
+# home page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# result page
+@app.route("/results", methods=["POST"])
+def results():  # todo custom not working, cannot get details by id in result page
+    query = request.form["query"]  # Get the raw user query from home page
+    global query_fix
+    query_fix = query
+    searchway = request.form["searchway"]
+    global docs
+    docs.clear()
+    doc_ids = []
+    doc_ids.clear()
+    # apply search way
+    if searchway == "bm25deault":   # BM25 + default analyzer
+        result = es.search(index=index_file, size=20, body={"query": {"match": {"content": query}}})
+        doc_ids = []
+        for doc in result['hits']['hits']:
+            doc_ids.append(doc['_id'])
+    elif searchway == "bm25custom":    # BM25 + custom analyzer
+        result = es.search(index=index_file, size=20, body={"query": {"match": {"custom_content": query}}})
+        doc_ids = []
+        for doc in result['hits']['hits']:
+            doc_ids.append(doc['_id'])
+    else:
+        if searchway == "fastText":    # Bert + default analyzer
+            encoder = EmbeddingClient(host="localhost", embedding_type="fasttext")
+        else:   # fastText + default analyzer
+            encoder = EmbeddingClient(host="localhost", embedding_type="sbert")
+        query_vector = encoder.encode([query], pooling="mean").tolist()[0]
+        # get result
+        result = es.search(index=index_file, size=20, body={"query": {"match": {"content": query}}})
+        doc_list = {}
+        for doc in result['hits']['hits']:
+            if searchway == "fastText": # Bert + default analyzer
+                embed_vec = doc['_source']['ft_vector']
+            else:   # fastText + default analyzer
+                embed_vec = doc['_source']['sbert_vector']
+            cs = cosine_similarity(query_vector, embed_vec)
+            doc_list[doc['_id']] = cs
+        ordered_doc = sorted(doc_list.items(), key=lambda kv: (kv[1], kv[0]))
+        doc_ids = [i[0] for i in ordered_doc]
+    # get result
+    for d in doc_ids:
+        doc = {}
+        doc['id'] = d
+        doc['title'] = es.get(index=index_file, id=d, doc_type="_all")['_source']['title']
+        doc['snippet'] = es.get(index=index_file, id=d, doc_type="_all")['_source']['content'][:150]
+        docs.append(doc)
+    # judge eight each page
+    if len(docs) <= 8:
+        return render_template("results.html", lst=docs, page=1, total=len(docs), last=True, query=query)
+    else:
+        return render_template("results.html", lst=docs[:8], page=1, total=len(docs), query=query)
+
+
+# "next page" to show more results
+@app.route("/results/<int:page_id>", methods=["GET"])
+def next_page(page_id):
+    if page_id * 8 < len(docs):
+        return render_template("results.html", lst=docs[(page_id - 1) * 8:page_id * 8], page=page_id,
+                               total=len(docs), query=query_fix)
+    else:
+        return render_template("results.html", lst=docs[(page_id - 1) * 8:len(docs)], page=page_id,
+                               total=len(docs), last=True, query=query_fix)
+
+
+# document page
+@app.route("/doc_data/<int:doc_id>")
+def doc_data(doc_id):
+    # get information
+    target = es.get(index=index_file, id=str(doc_id), doc_type="_all")
+    doc = target['_source']
+    title = doc['title']
+    author = doc['author']
+    main = doc["content"]
+    date = doc["date"]
+    annotation = doc["annotation"]
+    return render_template("doc.html", title=title, author=author, date=date, main=main, annotation=annotation)
+
+
+def dot(l1, l2):
+    return sum(a*b for a, b in zip(l1, l2))
+
+
+def cosine_similarity(a, b):
+    return dot(a, b) / ((dot(a, a) ** .5) * (dot(b, b) ** .5))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
